1def67f427bfafca829c463ab720cdb574164784
shweta777 <47916414+shweta777@users.noreply.github.com>
2019-02-26 10:51:45 +0530

    Update PatientsController.cs

--- RestApi/Controllers/PatientsController.cs
+++ RestApi/Controllers/PatientsController.cs
@@ -1,19 +1,16 @@
-﻿using RestApi.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Web.Helpers;
 using System.Web.Http;
+using RestApi.Interfaces;
+using RestApi.Models;
 
 namespace RestApi.Controllers
 {
     public class PatientsController : ApiController
     {
-
         private readonly IPatientsService patientsService;
         private readonly IEpisodesService episodesService;
         public PatientsController(IPatientsService patientsService, IEpisodesService episodesService)
@@ -21,29 +18,29 @@ namespace RestApi.Controllers
             this.patientsService = patientsService;
             this.episodesService = episodesService;
         }
-        
+
         [HttpGet]
         public HttpResponseMessage Get(int patientId)
-        {    
+        {
             if (patientId != int.MinValue)
             {
                 try
                 {
                     var patients = this.patientsService.GetPatients();
                     var patient = patients.Where(p => p.PatientId == patientId).FirstOrDefault();
-                    if (patient!=null)
+                    if (patient != null)
                     {
                         patient.Episodes = episodesService.GetEpisodes().Where(e => e.PatientId == patient.PatientId).ToList();
                         return Request.CreateResponse(HttpStatusCode.OK, patient);
                     }
-                    return Request.CreateResponse(HttpStatusCode.NotFound, $"Patient not found with patientId :{ patientId }.");                    
+                    return Request.CreateResponse(HttpStatusCode.NotFound, $"Patient not found with patientId :{ patientId }.");
                 }
                 catch (Exception e)
                 {
                     return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
-                } 
+                }
             }
             return Request.CreateResponse(HttpStatusCode.BadRequest);
-        } 
+        }
     }
 }
